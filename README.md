ff188828ffbcc224b7da35dc701feb7c3a9faa53
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2023-02-25 23:16:00 +0330

    Initials

---
+++ README.md
@@ -0,0 +1,16 @@
+# git-fame-coefficient
+
+Pretty-print git repository collaborators sorted by contributions, with coefficients for `ins`, `del`, `surv`.
+
+Developed by [Shahab Movahhedi](https://shmovahhedi.com)
+
+Install Python and pip, and then install [`git-fame` by casperdcl](https://github.com/casperdcl/git-fame) with:
+
+```
+pip install git-fame
+```
+
+And after closing and reopening the terminal, run:
+```
+npx git-fame-coefficient
+```
---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+import { execSync } from "child_process";
+import prompt from "prompt";
+
+let Total_X = 0,
+	Total_LOC = 0,
+	Result = [],
+	queries = [
+		{
+			command:
+				'git fame --loc=surv -w -M -C -C --format=json -s --log=FATAL --incl="src/.*.(php|html|scss|css|js|ts|jsx|tsx)$"',
+			x: 7,
+		},
+		{
+			command:
+				'git fame --loc=ins -w -M -C -C --format=json -s --log=FATAL --incl="src/.*.(php|html|scss|css|js|ts|jsx|tsx)$"',
+			x: 5,
+		},
+		{
+			command:
+				'git fame --loc=del -w -M -C -C --format=json -s --log=FATAL --incl="src/.*.(php|html|scss|css|js|ts|jsx|tsx)$"',
+			x: 1,
+		},
+	];
+
+queries.forEach((query) => {
+	Total_X += query.x;
+	let stdout = execSync(query.command);
+	let data = JSON.parse(stdout.toString());
+	data.data.forEach((i) => {
+		let found = Result.findIndex((el) => el.name === i[0]);
+		if (found > -1) {
+			Result[found].loc += i[1] * query.x;
+		} else {
+			Result.push({
+				name: i[0],
+				loc: i[1] * query.x,
+			});
+		}
+		Total_LOC += i[1] * query.x;
+	});
+});
+
+Total_LOC /= Total_X;
+Result.forEach((person) => {
+	person.loc = (person.loc / Total_X).toFixed();
+	person.loc_percent = +((person.loc / Total_LOC) * 100).toFixed(3);
+});
+
+Result.sort((a, b) => parseFloat(b.loc) - parseFloat(a.loc));
+
+prompt.start();
+prompt.get(["total_project_money", "gitfame_money_percent"], (err, result) => {
+	if (err) return console.table(Result);
+
+	if (result.total_project_money > 0) {
+		let gitfame_money_percent = result.gitfame_money_percent > 0 ? result.gitfame_money_percent : 100,
+			gitfame_money = (result.total_project_money * gitfame_money_percent) / 100;
+
+		Result.forEach((person) => {
+			person.money = ((gitfame_money * person.loc_percent) / 100).toFixed().toLocaleString(); // "en-US"
+		});
+	}
+
+	console.table(Result);
+});
